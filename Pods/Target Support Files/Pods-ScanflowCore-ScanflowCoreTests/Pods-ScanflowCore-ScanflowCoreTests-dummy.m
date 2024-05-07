503791aa61b445bca7e13f7975b72afb93f334b3
kalaimathi03 <kalaimathi.k@optisolbusiness.com>
2024-05-07 18:02:06 +0530

    remove source file

--- Pods/Target Support Files/Pods-ScanflowCore-ScanflowCoreTests/Pods-ScanflowCore-ScanflowCoreTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScanflowCore_ScanflowCoreTests : NSObject
-@end
-@implementation PodsDummy_Pods_ScanflowCore_ScanflowCoreTests
-@end
--- Pods/Target Support Files/Pods-ScanflowCore/Pods-ScanflowCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScanflowCore : NSObject
-@end
-@implementation PodsDummy_Pods_ScanflowCore
-@end
